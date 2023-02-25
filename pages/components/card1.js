6b5ca5ba78278195a3a759b220f768e2cd0c6014
Sahil K <skhunt125@gmail.com>
2023-02-24 21:21:10 -0600

    Working on components

--- pages/components/card1.js
+++ pages/components/card1.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Avatar, Button, Card} from 'react-native-paper';
+// import pic1 from "assets/baseline_account_circle_black_24dp.png";
+// import yuh from assets\splash.png
 
-const LeftContent = props => <Avatar.Icon {...props} icon="tree" />
+
+const LeftContent = props => <Avatar.Icon {...props} icon="human" />
 
 const Card1 = () => (
   <Card>
---
+++ pages/components/card2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const LeftContent = props => <Avatar.Icon {...props} icon="human" />
+
+const Card2 = () => (
+    <Text>Hello</Text>
+);
+
+export default Card2;
