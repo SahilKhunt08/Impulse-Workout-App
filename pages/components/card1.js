9c84af1ddd0e70cfd6e148b42c205bbcfe2c60bc
Sahil K <skhunt125@gmail.com>
2023-02-25 20:05:23 -0600

    Changes

--- pages/components/card1.js
+++ pages/components/card1.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Avatar, Button, Card} from 'react-native-paper';
 // import pic1 from "assets/baseline_account_circle_black_24dp.png";
@@ -9,30 +8,18 @@ import { Avatar, Button, Card} from 'react-native-paper';
 const LeftContent = props => <Avatar.Icon {...props} icon="human" />
 
 const Card1 = () => (
-  <Card style={styles.container}>
-    <View>
+  <Card>
     <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
-    </View>
-    <View>
+    <Card.Content>
+      <Text variant="titleLarge">Card title</Text>
+      <Text variant="bodyMedium">Card content</Text>
+    </Card.Content>
+    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
     <Card.Actions>
       <Button>Cancel</Button>
       <Button>Ok</Button>
     </Card.Actions>
-    </View>
   </Card>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    // display: 'flex',
-    // flex: 1,
-    // backgroundColor: '#429692',
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignContent: 'start',
-    maxHeight: 200,
-    width: 350,
-  },
-});
 
 export default Card1;
