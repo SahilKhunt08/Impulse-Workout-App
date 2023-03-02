af21b909a491a2ed3437df81ac9870355efcf0f9
Sahil K <skhunt125@gmail.com>
2023-03-02 08:30:45 -0600

    Added firebase and login page component

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+import firebase from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB5SWr-2KOZT855ie79W4tw2ps_bdak9sw",
+    authDomain: "impulse-workout-app.firebaseapp.com",
+    projectId: "impulse-workout-app",
+    storageBucket: "impulse-workout-app.appspot.com",
+    messagingSenderId: "134273052545",
+    appId: "1:134273052545:web:a6c2b681a304f3a5058280",
+    measurementId: "G-74PDHEYD5V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export {db};
--- pages/page3.js
+++ pages/page3.js
@@ -1,12 +1,82 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
-// import './pages/page3.css'
-// import "./pages/allPages.css"
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity } from "react-native";
+const handleLogin = () => {
+  console.log("fdd")
+}
 
 export default function Page3() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Page3</Text>
-    </View>
-  )
+    <View style={styles.container}>
+      {/* <Image style={styles.image} source={require("./assets/log2.png")} />  */}
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email."
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        /> 
+      </View> 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password."
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        /> 
+      </View> 
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text> 
+      </TouchableOpacity> 
+      <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+        <Text style={styles.loginText}>LOGIN</Text> 
+      </TouchableOpacity> 
+    </View> 
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#f6f3f9',
+  },
+  image: {
+    marginBottom: 40,
+  },
+  inputView: {
+    backgroundColor: "#FFC0CB",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+    
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF1493",
+  },
+});
