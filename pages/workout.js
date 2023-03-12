b995424668fb0481decbd882f06169a30ba14af5
Sahil K <skhunt125@gmail.com>
2023-03-12 16:22:56 -0500

    API done, starting to work on saving exercises to playlists.

--- pages/workout.js
+++ pages/workout.js
@@ -7,27 +7,21 @@ import { Card } from 'react-native-paper';
 export default function Workout() {
 
   const [exerciseArr, setExerciseArr] = useState([
-    // {id: "0", equipment: '111dumbbell', muscle: "444biceps"},
-    // {id: "1", equipment: '222barbell', muscle: "555biceps"},
-    // {id: "2", equipment: '333machine', muscle: "666biceps"},
     {id: "0", equipment: 'equipment1', muscle: "muscle1"},
     {id: "1", equipment: 'equipment2', muscle: "muscle2"},
     {id: "2", equipment: 'equipment3', muscle: "muscle3"},
   ]);
 
-  const removeElement = (index) => {
-    console.log("remove exercise")
+  const saveExercise = (index) => {
+    console.log("Save Exercise")
     // const newFruits = fruits.filter((_, i) => i !== index);
     const newExerciseArr = exerciseArr.filter(a => a.id !== index);
     // console.log(index);
     setExerciseArr(newExerciseArr);
   };
 
-  const [search, setSearch] = useState("biceps");
-  const [counter, setCount] = useState(3);
-
   const addInfo = () => {
-    console.log("yuhgf");
+    console.log("Adding Info");
     // fruits.push({
     //   id: counter, equipment: "tools", muscle: "lats",
     // });
@@ -41,43 +35,57 @@ export default function Workout() {
     const options = {
       method: 'GET',
       url: 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises',
-      params: {muscle: {search}},
+      params: {muscle: search},
       headers: {
         'X-RapidAPI-Key': 'd1b21c7c74msh10859b8b93cc3adp10d1c8jsncacbeda663df',
         'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
       }
     };
     axios.request(options).then(function (response) {
-      console.log(response.data);
+      // console.log(response.data);
       const allData = response.data;
+      console.log("----------------------");
+      // setExerciseArr(exerciseArr => [...exerciseArr, allData]);
+      for(var i = 0; i < 10; i++){
+        allData[i].id = counter + i;
+      }
+      setCount(counter + 10);
+      setExerciseArr(allData);
+      // console.log(exerciseArr);
 
     }).catch(function (error) {
       console.error(error);
     }); 
-
   }
 
+  const [search, setSearch] = useState("biceps");
+  const [counter, setCount] = useState(3);
   const [modalVisible, setModalVisible] = useState(false);
   const [modalDirections, setModalDirections] = useState("");
+  const [modalInfo, setModalInfo] = useState("");
 
   return (
     <View style={styles.container}>
 
-      <View style={styles.inputView}>
+      <View style={styles.searchView}>
         <TextInput
-          style={styles.TextInput}
+          style={styles.searchInput}
           placeholder="Search Workout"
           placeholderTextColor="#003f5c"
           onChangeText={(search) => setSearch(search)}
         /> 
+        <TouchableOpacity style={styles.searchButton} onPress={callAPI}>
+          <Text>Call API</Text>
+        </TouchableOpacity>
       </View> 
-      <TouchableOpacity style={styles.button1} onPress={callAPI}>
-        <Text>Call API</Text>
-      </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button1} onPress={() => addInfo()}>
+      {/* <TouchableOpacity style={styles.button2} onPress={() => addInfo()}>
         <Text>Add Info</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
+
+      <View style={styles.playlist}>
+        <Text>Save to playlist</Text>
+      </View>
 
       <Modal
         animationType="slide"
@@ -89,11 +97,12 @@ export default function Workout() {
         }}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
+            <Text style={styles.modalText}>{modalInfo}</Text>
             <Text style={styles.modalText}>{modalDirections}</Text>
               <Pressable
-                style={[styles.button, styles.buttonClose]}
+                style={[styles.modalButton, styles.buttonClose]}
                 onPress={() => setModalVisible(!modalVisible)}>
-                <Text style={styles.textStyle}>Hide Modal</Text>
+                <Text style={styles.textStyle}>Close Directions</Text>
               </Pressable>
           </View>
         </View>
@@ -101,20 +110,27 @@ export default function Workout() {
       
       <ScrollView style={styles.scrollStyle}>
       <View style={styles.container}>
-        {exerciseArr.map((hello, index) => (
-          <View key={index} style={{backgroundColor: "white", marginTop: 10, }}>
-            <Text> {hello.muscle + " | " + hello.equipment + " | " + hello.id} </Text>
-            <TouchableOpacity style={styles.button1} onPress={() => removeElement(hello.id)}>
-              <Text> Remove </Text>
-            </TouchableOpacity>
-        <Pressable
-          style={[styles.button, styles.buttonOpen]}
-          onPress={() => {
-            setModalVisible(true);
-            setModalDirections(hello.muscle + " | " + hello.equipment)
-          }}>
-          <Text style={styles.textStyle}>Details</Text>
-        </Pressable>
+        {exerciseArr.map((info, index) => (
+          <View key={index} style={styles.workoutCard}>
+            <Text> {info.name + " — " + info.muscle + " — " + info.id} </Text>
+            <View style={styles.buttonView}>
+              <TouchableOpacity style={styles.cardButton} onPress={() => saveExercise(info.id)}>
+                <Text> Save </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.cardButton]}
+                onPress={() => {
+                setModalVisible(true);
+                setModalDirections(info.instructions)
+                setModalInfo(
+                  "Name: " + info.name + " \n" + 
+                  "Muscle: " + info.muscle + " \n " + 
+                  "Equipment: " + info.equipment + " \n " +
+                  "Type: " + info.type + " — " + info.difficulty)
+                }}>
+                <Text>Details</Text>
+              </TouchableOpacity>
+            </View>
           </View>
           ))}
         </View>
@@ -124,6 +140,17 @@ export default function Workout() {
 }
 
 const styles = StyleSheet.create({
+
+  playlist: {
+    backgroundColor: "cyan",
+    borderRadius: 5,
+    borderWidth: 3,
+    alignItems: "center",
+    width: "90%",
+    height: "30%"
+  },
+
+  //————————————————————————————————————————————————————————————————
   container: {
     flex: 1,
     backgroundColor: "#fff",
@@ -136,22 +163,84 @@ const styles = StyleSheet.create({
     padding: 10,
     marginTop: 5,
   },
-  TextInput: {
+  button2: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    marginTop: 5,
+    borderRadius: 5,
+  },
+//————————————————————————————————————————————————————————————————
+  searchView: {
+    // width: "100%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  searchInput: {
+    backgroundColor: "#FFC0CB",
+    borderRadius: 20,
+    maxWidth: "75%", 
+    marginRight: 10,
     height: 50,
     flex: 1,
     padding: 10,
-    width: 200,
     marginLeft: 5,
+    bordercolor: "black",
+    borderWidth: 3,
   },
-  inputView: {
-    backgroundColor: "#FFC0CB",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "left",
-    marginTop: 20,
+  searchButton: {
+    backgroundColor: "white",
+    padding: 10,
+    bordercolor: "black",
+    borderRadius: 5,
+    bordercolor: "black",
+    borderWidth: 3,
+    marginRight: 5,
   },
+//————————————————————————————————————————————————————————————————
+  workoutCard: {
+    marginTop: 10,
+    width: "90%",
+    backgroundColor: "white",
+    bordercolor: "black",
+    borderWidth: 2,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignContent: "center",
+
+  }, 
+  buttonView: {
+    alignContent: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  cardButton: {
+    backgroundColor: "lightblue",
+    padding: 5,
+    width: "30%",
+    borderRadius: 5,
+    margin: 10,
+    height: 35,
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalButton: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+//————————————————————————————————————————————————————————————————
+
   playingSpace: {
     backgroundColor: 'white',
     borderColor: 'blue',
@@ -190,12 +279,6 @@ const styles = StyleSheet.create({
       padding: 10,
       elevation: 2,
     },
-    buttonOpen: {
-      backgroundColor: '#F194FF',
-    },
-    buttonClose: {
-      backgroundColor: '#2196F3',
-    },
     textStyle: {
       color: 'white',
       fontWeight: 'bold',
@@ -206,9 +289,10 @@ const styles = StyleSheet.create({
       textAlign: 'center',
     },
     scrollStyle: {
+      marginTop: 10,
       flex: 1,
-      backgroundColor: "cyan",
-      maxHeight: 400,
+      // backgroundColor: "cyan",
+      maxHeight: 495,
     }
 
 });
