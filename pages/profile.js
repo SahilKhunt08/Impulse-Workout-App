0b6cff3c8eaad399180d15177caf964a865aff8b
Sahil K <skhunt125@gmail.com>
2023-03-14 19:38:59 -0500

    Started profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,38 @@
-import React from 'react'
-import { SafeAreaView, Text, View, StyleSheet, Image } from 'react-native';
+import React, { useState } from 'react'
+import { TouchableOpacity, Text, View, StyleSheet, Image, TextInput} from 'react-native';
 import { Card } from 'react-native-paper';
 import { Avatar } from '@rneui/themed';
+import { auth } from './firebase';
+import {db} from './firebase';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { addDoc, getDoc, doc, enableNetwork, setDoc, getCountFromServer, collection, getDocs, namedQuery, query} from "firebase/firestore"; 
 
 export default function Profile() {
+
+  const [username, setUsername] = useState("");
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  async function saveProfile() {
+    const allDocInfo = [];
+    const docRef1 = doc(db, "accounts", user.uid);
+    const docSnap = await getDoc(docRef1);
+
+    if (docSnap.exists()) {
+      allDocInfo[0] = docSnap.data().var1; //name of field
+      allDocInfo[1] = docSnap.data().var2; //name of field
+      allDocInfo[2] = docSnap.data().username;
+    } else {
+      console.log("No such document!");
+    }
+
+    const docRef2 = await setDoc(doc(db, "accounts", user.uid), {
+      var1: allDocInfo[0],
+      var2: allDocInfo[1],
+      username: username,
+    });
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profilePicture}>
@@ -14,11 +43,17 @@ export default function Profile() {
         ></Avatar>
       </View>
       <Card>
-        <Text style={styles.paragraph}>
-          Name Name
-        </Text>
+      <TextInput
+          style={styles.paragraph}
+          placeholder="Username"
+          placeholderTextColor="#003f5c"
+          onChangeText={(username) => setUsername(username)}
+        /> 
         {/* <Image source={require('../assets/person1.png')}></Image> */}
       </Card>
+      <TouchableOpacity style={styles.button2} onPress={saveProfile}>
+          <Text> Save </Text>
+        </TouchableOpacity>
     </View>
   )
 }
@@ -41,5 +76,19 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     padding: 20
+  },
+  button1: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginTop: 5,
+  },
+  button2: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    marginTop: 5,
+    marginHorizontal: 5,
+    borderRadius: 5,
   },
 });
